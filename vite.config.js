df7af06ac6fff7ab670fade32d5d529150ee56cb
unknown <wildanibnujamil30@gmail.com>
2025-10-28 13:47:02 +0700

    stres

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss(),],
+  base: '/',
+  build: {
+    outDir: 'dist'
+  }
 })
