75761a48609a4471109277bf9550aab608082854
Wildan Ibnu Jamil <wildanibnujamil30@gmail.com>
2025-10-28 21:02:02 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -154,23 +154,59 @@ function App() {
       <div className="kontak mt-32 sm:p-10 p-0" id="kontak">
         <h1 className="text-4xl mb-2 font-bold text-center" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true" >Kontak</h1>
         <p className="text-base/loose text-center mb-10 opacity-50" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300" data-aos-once="true" >Mari Tehubung Dengan Saya</p>
-        <form action="https://formsubmit.co/wildsketch30@gmail.com" method="POST" className="bg-zinc-800 p-10 w-fit w-full mx-auto rounded-md" autoComplete="off" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500" data-aos-once="true" >
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          className="bg-zinc-800 p-10 w-fit w-full mx-auto rounded-md"
+          autoComplete="off"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          data-aos-delay="500"
+          data-aos-once="true"
+        >
           <div className="flex flex-col gap-6">
             <div className="flex flex-col gap-2">
               <label className="font-semibold">Nama Lengkap</label>
-              <input type="text" name="nama" placeholder="Masukkan Nama..." className="border border-zinc-500 p-2 rounded-md" required />
+              <input
+                type="text"
+                name="nama"
+                placeholder="Masukkan Nama..."
+                className="border border-zinc-500 p-2 rounded-md"
+                required
+              />
             </div>
             <div className="flex flex-col gap-2">
               <label className="font-semibold">Email</label>
-              <input type="email" name="email" placeholder="Masukkan Email..." className="border border-zinc-500 p-2 rounded-md" required />
+              <input
+                type="email"
+                name="email"
+                placeholder="Masukkan Email..."
+                className="border border-zinc-500 p-2 rounded-md"
+                required
+              />
             </div>
             <div className="flex flex-col gap-2">
-              <label htmlFor="pesan" className="font-semibold" >Pesan</label>
-              <textarea name="pesan" id="pesan" cols="45" rows="7" placeholder="Pesan..." className="border border-zinc-500 p-2 rounded-md" required ></textarea>
+              <label htmlFor="pesan" className="font-semibold">Pesan</label>
+              <textarea
+                name="pesan"
+                id="pesan"
+                cols="45"
+                rows="7"
+                placeholder="Pesan..."
+                className="border border-zinc-500 p-2 rounded-md"
+                required
+              ></textarea>
             </div>
             <div className="text-center">
-              <button type="submit" className="bg-violet-700 p-3 rounded-lg w-full cursor-pointer border border-zinc-600 hover:bg-violet-600">Kirim Pesan</button>
+              <button
+                type="submit"
+                className="bg-violet-700 p-3 rounded-lg w-full cursor-pointer border border-zinc-600 hover:bg-violet-600"
+                disabled={loading}
+              >
+                {loading ? "Mengirim..." : "Kirim Pesan"}
+              </button>
             </div>
+            {message && <p className="text-center mt-2 text-white">{message}</p>}
           </div>
         </form>
       </div>
