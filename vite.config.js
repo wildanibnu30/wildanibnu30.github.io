3a2e9d7c2a5c2551dc3e85456febaa445a11a7fc
unknown <wildanibnujamil30@gmail.com>
2025-10-28 18:38:45 +0700

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-  base: '/',
+  plugins: [react(), tailwindcss()],
+  base: '/', // Untuk username.github.io, base tetap '/'
   build: {
     outDir: 'dist'
   }
